0eb4f71db1960ec5cf47ac9bd6783eb2025d4357
Luebu <maximilian.luecker@hebk.de>
2022-02-11 12:58:13 +0100

    Florian carried uns alla

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 import Databse.Connector;
+import eu.cr4zyfl1x.logger.LogType;
+import eu.cr4zyfl1x.logger.Logger;
+
+import java.awt.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("HhhhEyy");
         Connector.connect();
 
+        new Menu();
+        initialzeLogger();
 
+    }
+    private static void initialzeLogger(){
+        Logger logger = new Logger("Sportorga", new Date(),"logs");
+        logger.load();
+        Logger.log(LogType.SYSTEM,"Initialized");
     }
 }
